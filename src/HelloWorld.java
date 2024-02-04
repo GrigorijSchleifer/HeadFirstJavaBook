9800414827e0f60a6e2946775e649ca1ec83a484
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2024-02-04 09:18:11 +0100

    Hello LOVE, it's been a long time since we first met ...

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String[] args){
+        int counter = 10_000;
+        // chars can hold only one character, it is not a String and is surrounded by single quotes
+        char charOne = '1';
+        String name = "Grigorij";
+
+        System.out.println("Hello, Grigorij!");
+    }
+}
