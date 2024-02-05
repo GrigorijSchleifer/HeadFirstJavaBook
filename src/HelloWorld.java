27f959cd67eec59ac29c38a1c3f7156d0bffae61
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2024-02-05 21:07:20 +0100

    Understanding static

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,18 +1,22 @@
+// classes are like files in Java
 class HelloWorld {
+    // primitive types (there are 8 primitive types in Java) and the start with a lower case
+    int counter = 10_000;
+    // chars can hold only one character, it is not a String and is surrounded by single quotes
+    char charOne = '1';
+    // String is not a primitive type
+    String name = "Grigorij";
+
+    // static variables and methods belong to the Class and not instances of Class
     public static void main(String[] args){
-        int counter = 10_000;
-        // chars can hold only one character, it is not a String and is surrounded by single quotes
-        char charOne = '1';
-        String name = "Grigorij";
-
-        System.out.println("Hello, Grigorij!");
+        HelloWorld collection_of_names = new HelloWorld();
+        collection_of_names.namePrint(name);
     }
+
+    public void namePrint(String name) {
+        System.out.println("My name is :" + name);
+    }
+
 }
 
-public class HolloWorldTwo {
-    public static void main(String[] args) {
-        System.out.println("Drunk and voodoo magic");
-        char a = 'a';
-        String name = "Grigorij";
-    }
-}
+    // see if I can use a non static method inside a static method
