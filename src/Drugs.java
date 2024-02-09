51fe6c9893fc8684987f854204cd0eaa9605bff0
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2024-02-09 05:07:26 +0100

    Still confusing static and scope issues

--- src/Drugs.java
+++ src/Drugs.java
@@ -3,6 +3,7 @@ public class Drugs {
     int concentration = 10;
 
     public void infusion() {
-        System.out.printf("%s has a concentration of %s micrograms", drug_name, concentration);
+        System.out.printf("%s has a concentration of %s micrograms \n", drug_name, concentration);
+        System.out.printf("%d is received by the getTestInteger method \n", HelloWorld.getTest_integer());
     }
 }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,21 @@
 // class declaration, always needed
 public class HelloWorld {
+    public static int test_integer = 1;
+
+    public static int getTest_integer(){
+        return test_integer;
+    }
+
     public static void main(String[] args){
+
         Drugs drug = new Drugs();
         drug.infusion();
+        System.out.printf("%d is from inside the main method \n", test_integer);
+
+        int number_int_parsed = (int) 1.3;
+        double number_double = 1.3;
+
+        System.out.printf("Parsed double to integer: %d and just double %f", number_int_parsed, number_double);
     }
 
 }
