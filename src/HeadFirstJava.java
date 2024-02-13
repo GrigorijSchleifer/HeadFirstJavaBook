9f4f5793f1d715246fd11eaa805ba7a37d98e2fe
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2024-02-13 19:56:13 +0100

    bottle game and first sharpen you pencils

--- src/HeadFirstJava.java
+++ src/HeadFirstJava.java
@@ -12,10 +12,15 @@ public class HelloWorld {
         drug.infusion();
         System.out.printf("%d is from inside the main method \n", test_integer);
 
-        int number_int_parsed = (int) 1.3;
+        int number_int_parsed =  (int) 10;
         double number_double = 1.3;
 
-        System.out.printf("Parsed double to integer: %d and just double %f", number_int_parsed, number_double);
+        System.out.printf("Parsed double to integer: %d and just double %f \n", number_int_parsed, number_double);
+
+        while(drug.concentration < 2) {
+            System.out.print();
+            number_int_parsed = number_int_parsed - 1;
+        }
     }
     // hello
 }
--- src/SharpenYourPens.java
+++ src/SharpenYourPens.java
@@ -1,9 +1,9 @@
 public class Drugs {
     String drug_name = "Noradrenaline";
-    int concentration = 10;
+    public int concentration = 10;
 
     public void infusion() {
         System.out.printf("%s has a concentration of %s micrograms \n", drug_name, concentration);
-        System.out.printf("%d is received by the getTestInteger method \n", HelloWorld.getTest_integer());
+        System.out.printf("%d is received by the getTestInteger method \n", HeadFirstJava.getTest_integer());
     }
 }
