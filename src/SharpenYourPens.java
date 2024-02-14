aed0ea07899eba7db80a50e7098294c49e68e2c1
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2024-02-14 07:10:59 +0100

    does it work?

--- src/SharpenYourPens.java
+++ src/SharpenYourPens.java
@@ -1,9 +1,35 @@
-public class Drugs {
-    String drug_name = "Noradrenaline";
-    public int concentration = 10;
+public class SharpenYourPens {
+    public void syp_1() {
+        int counter = 0;
 
-    public void infusion() {
-        System.out.printf("%s has a concentration of %s micrograms \n", drug_name, concentration);
-        System.out.printf("%d is received by the getTestInteger method \n", HeadFirstJava.getTest_integer());
+        while (counter < 2) {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            counter = counter + 1;
+        }
+        if (counter == 2) {
+            System.out.println("Do");
+        }
+    }
+
+    public void BottleSong(){
+        int bottles = 10;
+        String word = "bottles";
+
+        while (bottles > 0) {
+            if (bottles == 1) {
+                word = "bottle"; // to correct for singular spelling
+            }
+
+            System.out.printf("%d " + word + " on the floor \n", bottles);
+            System.out.println("And if one green bottle should accidentally fall, ");
+            bottles = bottles - 1;
+
+            if (bottles > 0) {
+                System.out.printf("There will be %d" + " green " + word + " on the floor \n\n", bottles);
+            } else {
+                System.out.println("There will not be green bottles on the floor");
+            }
+        }
     }
 }
