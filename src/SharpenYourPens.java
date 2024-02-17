572aecb8e998ebefe24ab02424f0e742f4c82a28
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2024-02-17 22:18:39 +0100

    free up staging area and switch to master

--- src/SharpenYourPens.java
+++ src/SharpenYourPens.java
@@ -75,6 +75,7 @@ public class SharpenYourPens {
     }
     // this should be a test if I can merge this comment to master and bring local master behind remote master
     // test didnt work
+    // lets try to stage (add) it first
     public void be_a_compiler(){
         int i = 5;
         while(i > 1) {
